284e92069e1f6bd92e42b2f010bbe62b23e484d8
Owner <Owner@10.240.209.218>
2014-05-28 20:30:47 -0400

    Merge origin/master

---
+++ Final/signup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>SaaS Project - Sign-up</title>
+         <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    <body>
+                
+        
+        <h1 id="logo"><span>&#x2728;</span>SaaS Project</h1>
+        
+        <fieldset>
+            <legend>Signup</legend>
+        <p> Already a member, <a href="login.php">Login</a></p>
+         <form name="mainform" action="#" method="post">
+            
+             
+                         <label>Web Site:</label> <input type="text" name="website" maxlength="30" /> <br />
+             
+            
+                          <label>Email:</label> <input type="text" name="email" /> <br />
+            
+                          <label>Password:</label> <input type="password" name="password" /> <br />
+               
+            <input type="submit" value="Submit" />
+                        
+        </form>
+        </fieldset>
+    </body>
+</html>
